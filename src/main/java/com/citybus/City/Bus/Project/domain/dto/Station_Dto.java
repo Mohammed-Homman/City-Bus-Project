f8a58c47c1762fff38c860585a439ad6c2d70897
Mohammed <mohamedhommane4@gmail.com>
2024-02-21 17:25:56 +0100

    Adding Station entity and dto.

---
+++ src/main/java/com/citybus/City/Bus/Project/domain/dto/Station_Dto.java
@@ -0,0 +1,20 @@
+package com.citybus.City.Bus.Project.domain.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Station_Dto {
+    private int station_id;
+    private String nom_station;
+    private BigDecimal latitude;
+    private BigDecimal longitude;
+
+}
---
+++ src/main/java/com/citybus/City/Bus/Project/domain/entities/Station_Entity.java
@@ -0,0 +1,26 @@
+package com.citybus.City.Bus.Project.domain.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Builder
+@Table(name = "Station")
+public class Station_Entity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "station_id_seq")
+    private int station_id;
+    private String nom_station;
+    private BigDecimal latitude;
+    private BigDecimal longitude;
+
+
+}
