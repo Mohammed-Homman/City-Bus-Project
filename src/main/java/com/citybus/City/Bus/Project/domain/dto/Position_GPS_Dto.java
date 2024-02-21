eb5787df03dcfa9d77075f07bdbbd8b70bfe752f
Mohammed <mohamedhommane4@gmail.com>
2024-02-21 17:45:43 +0100

    Adding Station entity and dto.

---
+++ src/main/java/com/citybus/City/Bus/Project/domain/dto/Position_GPS_Dto.java
@@ -0,0 +1,21 @@
+package com.citybus.City.Bus.Project.domain.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Position_GPS_Dto {
+    private int id_position;
+    private BigDecimal latitude;
+    private BigDecimal longitude;
+    private LocalDateTime temps_de_localisation;
+
+}
---
+++ src/main/java/com/citybus/City/Bus/Project/domain/entities/Position_GPS_Entity.java
@@ -0,0 +1,25 @@
+package com.citybus.City.Bus.Project.domain.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@Table(name = "Position_GPS")
+public class Position_GPS_Entity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE,generator = "position_gps_id_seq")
+    private int id_position;
+    private BigDecimal latitude;
+    private BigDecimal longitude;
+    private LocalDateTime temps_de_localisation;
+}
