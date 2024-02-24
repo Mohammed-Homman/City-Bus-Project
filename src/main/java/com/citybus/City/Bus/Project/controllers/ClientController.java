beaab4e926072c4d14969f4db31cc2b248703d36
Mohammed <mohamedhommane4@gmail.com>
2024-02-24 17:12:15 +0100

    I have implemented All the Repositories, you can use them now si Amine.

--- src/main/java/com/citybus/City/Bus/Project/controllers/ClientController.java
+++ src/main/java/com/citybus/City/Bus/Project/controllers/ClientController.java
@@ -9,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @RestController
@@ -32,6 +33,15 @@ public class ClientController {
         List<ClientEntity> clientEntities = clientService.findAll();
         return clientEntities.stream().map(clientMapper::mapTo).collect(Collectors.toList());
     }
+    @GetMapping(path = "/Client/{id}")
+    public ResponseEntity<ClientDto> getClient(@PathVariable("id") int id){
+        Optional<ClientEntity> foundClient = clientService.findOne(id);
+        return foundClient.map(clientEntity -> {
+            ClientDto clientDto = clientMapper.mapTo(clientEntity);
+            return new ResponseEntity<>(clientDto, HttpStatus.OK);
+        }).orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    }
+
     @PutMapping(path = "/Client/{id}")
     public ResponseEntity<ClientDto> fullUpdateClient(
             @PathVariable("id") int id,
@@ -47,6 +57,22 @@ public class ClientController {
                     clientMapper.mapTo(savedClientEntity),
                     HttpStatus.OK);
         }
+    @PatchMapping(path = "/Client/{id}")
+    public ResponseEntity<ClientDto> partialUpdate(
+            @PathVariable("id") int id,
+            @RequestBody ClientDto clientDto
+    ){
+        if(!clientService.isExists(id)){
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
+        ClientEntity clientEntity = clientMapper.mapFrom(clientDto);
+        ClientEntity updatedClient = clientService.partialUpdate(id, clientEntity);
+        return new ResponseEntity<>(
+                clientMapper.mapTo(updatedClient),
+                HttpStatus.OK);
+    }
+
 
 
 
--- src/main/java/com/citybus/City/Bus/Project/services/implServices/ClientServiceImpl.java
+++ src/main/java/com/citybus/City/Bus/Project/services/implServices/ClientServiceImpl.java
@@ -6,6 +6,7 @@ import com.citybus.City.Bus.Project.services.ClientService;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;
 
@@ -28,7 +29,35 @@ public class ClientServiceImpl implements ClientService {
     }
 
     @Override
+    public Optional<ClientEntity> findOne(int id) {
+        return clientRepository.findById(id);
+    }
+
+    @Override
+    public ClientEntity partialUpdate(int id, ClientEntity clientEntity) {
+        clientEntity.setId(id);
+
+        return clientRepository.findById(id).map(existingClient ->{
+            Optional.ofNullable(clientEntity.getNom()).ifPresent(existingClient::setNom);
+            Optional.ofNullable(clientEntity.getPrenom()).ifPresent(existingClient::setPrenom);
+            Optional.ofNullable(clientEntity.getEmail()).ifPresent(existingClient::setEmail);
+            Optional.ofNullable(clientEntity.getTele()).ifPresent(existingClient::setTele);
+            Optional.ofNullable(clientEntity.getAdresse()).ifPresent(existingClient::setAdresse);
+            Optional.ofNullable(clientEntity.getEmploi()).ifPresent(existingClient::setEmploi);
+            Optional.ofNullable(clientEntity.getCni()).ifPresent(existingClient::setCni);
+            Optional.ofNullable(clientEntity.getMot_de_passe()).ifPresent(existingClient::setMot_de_passe);
+            Optional.ofNullable(clientEntity.getDateNaissance()).ifPresent(existingClient::setDateNaissance);
+            Optional.ofNullable(clientEntity.getSex()).ifPresent(existingClient::setSex);
+            Optional.ofNullable(clientEntity.getDateInscription()).ifPresent(existingClient::setDateInscription);
+
+            return clientRepository.save(existingClient);
+        }).orElseThrow(() -> new RuntimeException("Client does not exist"));
+    }
+    ;
+
+    @Override
     public boolean isExists(int id) {
         return clientRepository.existsById(id);
     }
+
 }
